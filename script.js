44d967c167d7458388b2cbb2cf486aba107aa6a7
Andrew Carey <andrew.r.carey@gmail.com>
2022-10-07 20:31:34 -0500

    added notes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
+
+    // This block adds up an allowance for each chore on the list and logs the total to the console
 let allowance = 0
     $('#choreCheckList :checkbox').change(function() {
-        // this will contain a reference to the checkbox   
         if (this.checked) {
             allowance += .5;
             console.log(`Mal's allowance is \$${allowance}`);
@@ -10,7 +11,7 @@ let allowance = 0
             allowance -= .5;
             console.log(`Mal's allowance is \$${allowance}`);
 
-            // the checkbox is now no longer checked
+            // the checkbox is now no longer checked and the allowance is removed
         }
     });
 });

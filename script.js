c08b6679f59fa368406d4fbb346cbdd9bbf3b953
Andrew Carey <andrew.r.carey@gmail.com>
2022-10-07 20:29:14 -0500

    created check boxes, created check events adding up allowance values

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('JS');
+$(document).ready(function(){
+let allowance = 0
+    $('#choreCheckList :checkbox').change(function() {
+        // this will contain a reference to the checkbox   
+        if (this.checked) {
+            allowance += .5;
+            console.log(`Mal's allowance is \$${allowance}`);
+            // the checkbox is now checked 
+        } else {
+            allowance -= .5;
+            console.log(`Mal's allowance is \$${allowance}`);
+
+            // the checkbox is now no longer checked
+        }
+    });
+});
+    
+    
